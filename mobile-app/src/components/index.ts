45bc14ed9dfdb282971ffdd814fcd7c10d81121d
Angelo Evangelista <angeloevan.ane@gmail.com>
2024-06-01 21:51:42 -0300

    chore: remove unused components

--- mobile-app/src/components/index.ts
+++ mobile-app/src/components/index.ts
@@ -1,6 +1,3 @@
-export * from './Collapsible';
-export * from './ExternalLink';
-export * from './ParallaxScrollView';
 export * from './ThemedText';
 export * from './InputField';
 export * from './Chip';
