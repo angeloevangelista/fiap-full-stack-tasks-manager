9926eb20d9fb5de1af6f16ff5edc33650e71a5a4
Angelo Evangelista <angeloevan.ane@gmail.com>
2024-06-20 08:35:08 -0300

    feat: add sqlite support

--- web-api/src/database/connection.ts
+++ web-api/src/database/connection.ts
@@ -2,19 +2,24 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 import { Task } from '../entities';
 
-const dataSourceOptions: DataSourceOptions = {
-  synchronize: true,
+const postgresDataSourceOptions: DataSourceOptions = {
   type: process.env.DB_ENGINE,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  logging: String(process.env.DB_LOG).toLowerCase() == 'true',
+} as DataSourceOptions;
+
+const sqlLiteDataSourceOptions: DataSourceOptions = {
+  type: "sqlite",
+  database: "tasks-manager.db",
 } as DataSourceOptions;
 
 const databaseSource = new DataSource({
-  ...dataSourceOptions,
+  ...(process.env.DB_ENGINE ? postgresDataSourceOptions : sqlLiteDataSourceOptions),
+  synchronize: true,
+  logging: String(process.env.DB_LOG).toLowerCase() == 'true',
   entities: [Task],
 });
 
